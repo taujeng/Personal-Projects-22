588430ec2b530bc6c489ca360cc7599cb24acea2
Tau <taujeng.1@gmail.com>
2022-04-15 09:29:27 +0800

    completed

--- React-BreakingBadAPI/breakingbad/src/App.js
+++ React-BreakingBadAPI/breakingbad/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import Header from "./components/UI/Header"
+import CharacterGrid from "./components/characters/CharacterGrid"
+import Search from "./components/UI/Search"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect( ()=> {
+    const fetchItems = async() => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems();
+  }, [query])
+
+
   return (
     <div className="container">
-     Hello
+     <Header />
+     <Search getQuery={(q)=> setQuery(q)} />
+     <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
